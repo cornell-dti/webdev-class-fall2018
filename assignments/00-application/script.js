90eca6b2f33a6e6aba0df2a7b24cd74706bdaf8c
Shea Belsky <shb95@cornell.edu>
2018-09-06 17:21:52 -0400

    First pass at application

---
+++ assignments/00-application/script.js
@@ -0,0 +1,5 @@
+/**
+ * Edit this file to get user input and submit it to the Yoda Speak API.
+ * All function and variable names are up to you.
+ * Assume users will be viewing your project in a modern web browser (Chrome/Firefox).
+ */
